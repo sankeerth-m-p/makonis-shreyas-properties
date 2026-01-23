814a6f8e12444c61034a2d45f77b93a31d0e7fac
Sankeerth Ajith <sankeerthajith3@gmail.com>
2026-01-23 16:37:27 +0530

    scroll to top

--- src/components/scrollToTop.jsx
+++ src/components/scrollToTop.jsx
@@ -1,17 +1,17 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { useLenis } from "lenis/react";
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
-  const lenis = useLenis();
 
   useEffect(() => {
-    if (lenis) {
-      // instantly jump to top (no animation)
-      lenis.scrollTo(0, { immediate: true });
-    }
-  }, [pathname, lenis]);
+    // force scroll to top on every route change
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant", // or "smooth"
+    });
+  }, [pathname]);
 
   return null;
 }
