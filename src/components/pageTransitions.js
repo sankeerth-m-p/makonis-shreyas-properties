85821c9e62b1c58a8a95b45ddcb41cd137cfb690
Sankeerth Ajith <sankeerthajith3@gmail.com>
2026-01-30 11:15:34 +0530

    removed page transiton temproralty

--- src/components/pageTransitions.js
+++ src/components/pageTransitions.js
@@ -1,10 +1,10 @@
 export const pageVariants = {
-  initial: { opacity: 0, scale: 1 },
+  initial: { opacity: 1, scale: 1 },
   animate: { opacity: 1, scale: 1 },
   exit: { opacity: 0, scale: 1 },
 };
 
 export const pageTransition = {
-  duration: 0.3,
-  ease: "easeOut",
+  duration: 0,
+  ease: "easeIn",
 };
